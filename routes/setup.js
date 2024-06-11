0e36f14e6c8c9ca2707c6246f28528c9414efb25
Erarshad <www.moarshad310@gmail.com>
2024-06-11 22:27:25 +0530

    added

--- routes/setup.js
+++ routes/setup.js
@@ -20,7 +20,7 @@ router.post("/user", async (req, res) => {
     if (userPlan.type === 'free') {
       //  expiry = new Date("January 1, 2099 01:15:00");//means lifetime
       //15 days free, plan 
-        expiry = new Date(expiry.setDate(expiry.getMonth() + 20));
+        expiry = new Date(expiry.setDate(expiry.getMonth() + 15));
 
     } else {
         let months = userPlan.validity_in_month;
